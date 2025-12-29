0ad3e5fa4e2dd51f1206ba6b37d7d021059096fa
Evgeniy Medvedev <jidckii@gmail.com>
2025-12-29 17:36:39 +0400

    update

--- publish.sh
+++ publish.sh
@@ -7,12 +7,10 @@ if ${DEBUG} ; then
     set -x
 fi
 
-SCRIPT_DIR="$(cd $(dirname "$0") && pwd)"
-
 CODEBERG_FORGEJO_TOKEN=${CODEBERG_FORGEJO_TOKEN:-""}
-FORGEJO_TOKEN=${FORGEJO_TOKEN}
+FORGEJO_TOKEN=${CODEBERG_FORGEJO_TOKEN}
 
-if [ -z "${CODEBERG_FORGEJO_TOKEN}" ]; then
+if [ -z "${FORGEJO_TOKEN}" ]; then
     echo "CODEBERG_FORGEJO_TOKEN is not set"
     exit 1
 fi
@@ -22,24 +20,24 @@ BASE_URL="https://codeberg.org"
 
 DEB_PKG=$(ls dist/*.deb)
 echo "Uploading ${DEB_PKG} to ${BASE_URL} for Debian ..."
-curl -sSL --user ${FORGEJO_USER}:${FORGEJO_TOKEN} \
-    --upload-file ${DEB_PKG} \
-    ${BASE_URL}/api/packages/${FORGEJO_USER}/debian/pool/stable/main/upload
+curl -sSL --user "${FORGEJO_USER}:${FORGEJO_TOKEN}" \
+    --upload-file "${DEB_PKG}" \
+    "${BASE_URL}/api/packages/${FORGEJO_USER}/debian/pool/stable/main/upload"
 
 RPM_PKG=$(ls dist/*.rpm)
 echo "Uploading ${RPM_PKG} to ${BASE_URL} for RHEL/SUSE ..."
-curl -sSL --user ${FORGEJO_USER}:${FORGEJO_TOKEN} \
-    --upload-file ${RPM_PKG} \
-    ${BASE_URL}/api/packages/${FORGEJO_USER}/rpm/upload
+curl -sSL --user "${FORGEJO_USER}:${FORGEJO_TOKEN}" \
+    --upload-file "${RPM_PKG}" \
+    "${BASE_URL}/api/packages/${FORGEJO_USER}/rpm/upload"
 
 echo "Uploading ${RPM_PKG} to ${BASE_URL} for ALT Linux ..."
-curl -sSL --user ${FORGEJO_USER}:${FORGEJO_TOKEN} \
-    --upload-file ${RPM_PKG} \
-    ${BASE_URL}/api/packages/${FORGEJO_USER}/alt/upload
+curl -sSL --user "${FORGEJO_USER}:${FORGEJO_TOKEN}" \
+    --upload-file "${RPM_PKG}" \
+    "${BASE_URL}/api/packages/${FORGEJO_USER}/alt/upload"
 
 ARCH_PKG=$(ls dist/*.pkg.tar.zst)
 echo "Uploading ${ARCH_PKG} to ${BASE_URL} for Arch Linux ..."
-curl -sSL -XPUT --user ${FORGEJO_USER}:${FORGEJO_TOKEN} \
+curl -sSL -XPUT --user "${FORGEJO_USER}:${FORGEJO_TOKEN}" \
     --header "Content-Type: application/octet-stream" \
     --data-binary "@${ARCH_PKG}" \
-    ${BASE_URL}/api/packages/${FORGEJO_USER}/arch/extras
+    "${BASE_URL}/api/packages/${FORGEJO_USER}/arch/extras"
